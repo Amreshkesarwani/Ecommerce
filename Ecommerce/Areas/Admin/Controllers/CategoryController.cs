03783861a82abf8d07e3031231ff7f42b158793c
Amresh Kesarwani <Amresh Kesarwani@AMRESH>
2021-03-21 00:10:47 +0530

    Add project files.

---
+++ Ecommerce/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class CategoryController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Add(int Id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Ecommerce/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
